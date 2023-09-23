8b5a0a4942a481046477e4ca62dcabac7bcf883d
Jessica Nguyen <jn3677@rit.edu>
2023-09-23 15:25:46 -0400

    FacialRec.py

--- FacialRec.py
+++ FacialRec.py
@@ -1,11 +1,13 @@
 import cv2
-
+import time
+import winsound  # Import winsound for Windows
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 
+last_detection_time = time.time()
 while True:
 
     _, img = cap.read()
@@ -15,13 +17,19 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-      #  cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
+        if len(eyes) > 0:
+            last_detection_time = time.time()
+          #  winsound.Beep(1000, 200)  # Play a sample sound (1000 Hz, 200 ms duration)
+
+    if (time.time() - last_detection_time) > 1.0:
+        winsound.Beep(500, 200)  # Play a different sample sound
+        print("Eyes not detected")
 
     cv2.imshow('img', img)
 
@@ -30,3 +38,4 @@ while True:
         break
 
 cap.release()
+cv2.destroyAllWindows()
